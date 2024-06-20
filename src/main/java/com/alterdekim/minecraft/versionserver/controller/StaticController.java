b0c8b273e843e34267d982b83fdb4fba8e0aff34
alterdekim <alterwain@protonmail.com>
2024-06-20 16:54:20 +0300

    more changes x4

--- src/main/java/com/alterdekim/minecraft/versionserver/controller/StaticController.java
+++ src/main/java/com/alterdekim/minecraft/versionserver/controller/StaticController.java
@@ -8,12 +8,10 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.util.MimeType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
